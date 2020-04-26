caf77887e0b3e4f7ecc48f7a8262081d516a4fd5
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-04-26 00:19:45 -0500

    WIP - basic card components and styling complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './Components/Card-List/Card-List.component'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({monsters: users}))
+
+  }
+
+  render() {
+    return (
+      <div className = 'App'>
+      <h1>Monsters Rolodex</h1>
+
+      <CardList monsters = {this.state.monsters}/>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
