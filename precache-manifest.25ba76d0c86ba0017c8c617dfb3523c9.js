1464a3fc7ac1778ba0573157b68a3f0807f78509
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-04-26 10:56:51 -0500

    Updates

---
+++ precache-manifest.25ba76d0c86ba0017c8c617dfb3523c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4de63e602218fd22227e5c8761705f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d63ffc0c6d29a4f97be5",
+    "url": "/monsters-rolodex/static/css/main.c8866b3c.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63ffc0c6d29a4f97be5",
+    "url": "/monsters-rolodex/static/js/main.335751f9.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
